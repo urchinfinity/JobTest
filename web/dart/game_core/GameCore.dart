39d74334812e4be52a47b23283360d3110e91001
Abby Yeh <radleaf@gmail.com>
2015-10-12 22:55:35 +0800

    files

---
+++ web/dart/game_core/GameCore.dart
@@ -0,0 +1,6 @@
+library GameCore;
+
+part 'src/choice.dart';
+part 'src/level.dart';
+part 'src/executor.dart';
+part 'src/util.dart';
---
+++ web/dart/game_core/src/level.dart
@@ -0,0 +1,9 @@
+part of GameCore;
+
+class Level {
+	List<Srting> dialogs;
+	String choice;
+	Choice usersChoice;
+	Map result;
+	List 
+}
