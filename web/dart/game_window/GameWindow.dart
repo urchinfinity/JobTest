ddb60bbcb717b85a96eedd8195ac1cdaff308bc3
Urchin <anabelxrita@gmail.com>
2015-10-13 18:52:39 +0800

    add storyline

--- web/dart/game_window/GameWindow.dart
+++ web/dart/game_window/GameWindow.dart
@@ -3,5 +3,7 @@ library GameWindow;
 import 'dart:html';
 import 'dart:async';
 
+import 'Fieldname.dart';
+
 part 'src/Dialog.dart';
-part 'src/Map.dart';
+part 'src/GameMap.dart';
--- web/dart/game_window/src/Dialog.dart
+++ web/dart/game_window/src/Dialog.dart
@@ -3,32 +3,71 @@ part of GameWindow;
 class Dialog {
 
 	DivElement _textbox;
+	DivElement _mask;
+	DivElement _img;
+	DivElement _contentParent;
+	DivElement _optionsParent;
 
 	Dialog() {
 		_textbox = querySelector('#dialog');
+		_mask = querySelector('#dialog_mask');
+		_img = querySelector('#dialog .image');
+		_contentParent = querySelector('#dialog .content');
+		_optionsParent = querySelector('#dialog .options');
 	}
 
-	void showDialog() {
-
+	void showDialog(int lines) {
+		_textbox.classes.remove('hidden');
+		_textbox.style.height = _px(TEXT_HEIGHT * lines);
+		new Timer(const Duration(milliseconds: DIALOG_ANIMATION_DURATION), () {
+			_mask.classes.remove('hidden');
+		});
 	}
 
 	void hideDialog() {
-
+		_mask.classes.add('hidden');
+		_textbox.style.height = _px(0);
+		new Timer(const Duration(milliseconds: DIALOG_ANIMATION_DURATION), () {
+			_textbox.classes.add('hidden');
+		});
 	}
 
 	void clearDialog() {
-
+		_img.classes.add('hidden');
+		_contentParent.children.clear();
+		_optionsParent.children.clear();
 	}
 
-	void showText(String text) {
-
+	void showContent(String content) {
+		ParagraphElement p = new ParagraphElement();
+		p.innerHtml = content;
+		_contentParent.children.add(p);
 	} 
 
-	void showTest(String question, List<String> options) {
+	void showOptions(List<String> options) {
+		for (int i = 0; i < OPTIONS_NUM; i++) {
+			ParagraphElement p = new ParagraphElement();
+			p.innerHtml = options[i];
+			p.id = '$i';
+			_optionsParent.children.add(p);
+		}
+	}
 
+	void startOptionsListener(Function action) {
+		List listeners = new List();
+		ElementList<ParagraphElement> options = querySelectorAll('#dialog .options p');
+		options.forEach((ParagraphElement option) {
+			var listener = option.onClick.listen((e) {
+				listeners.forEach((lstnr) => lstnr.cancel());
+				action(int.parse(option.id));
+			});
+			listeners.add(listener);
+		});
 	}
 
 	void playSound(String soundID) {
 
 	}
+
+	String _px(int num) => '${num}px';
 }
